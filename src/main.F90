68247fa0fa761b126f34df53381791f85be5bb7a
Ron Rahaman <rahaman@mcs.anl.gov>
2017-03-10 08:17:50 -0600

    Demonstrates HDF5 error when using new MPI communicator

--- src/main.F90
+++ src/main.F90
@@ -10,11 +10,33 @@ program main
   use simulation,        only: run_simulation
   use volume_calc,       only: run_volume_calculations
 
+  use mpi
+
   implicit none
+
+  integer :: world_group               ! Group belonging to MPI_COMM_WORLD
+  integer :: openmc_comm, openmc_group ! A new group/comm for running OpenMC
 
   ! Initialize run -- when run with MPI, pass communicator
 #ifdef MPI
-  call openmc_init(MPI_COMM_WORLD)
+
+  call MPI_Init(mpi_err)
+
+  ! Make a new communicator from a subset of procs.  For demonstrative purposes, we make a
+  ! communicator from just one proc.
+  call MPI_Comm_group(MPI_COMM_WORLD, world_group, mpi_err)
+  call MPI_Group_incl( &
+      world_group, &
+      1, &
+      (/ 0 /), &
+      openmc_group, &
+      mpi_err)
+  call MPI_Comm_create(MPI_COMM_WORLD, openmc_group, openmc_comm, mpi_err)
+
+  if (openmc_comm /= MPI_COMM_NULL) then
+    call openmc_init(openmc_comm)
+  endif
+
 #else
   call openmc_init()
 #endif
